eb4884b274e99713a6dd78c9302cf8cf1577fdec
santiago chinellato <santiagochinellato@gmail.com>
2021-07-06 14:55:11 -0300

    cambios

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { ItemCount } from "../ItemCount/ItemCount";
 import "./ItemDetail.css";
 
--- src/containers/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer/ItemDetailContainer.jsx
@@ -4,22 +4,6 @@ import ItemDetail from "../../components/ItemDetail/ItemDetail";
 import { useParams } from "react-router";
 import productos from "../../json/Productos.json";
 
-// const ItemDetailContainer = (props) => {
-//   const [descripcion, setDescripcion] = useState([]);
-//   useEffect(async () => {
-//     const response = await fetch("./json/productos.json");
-//     const result = await response.json();
-//     setDescripcion(result[0]);
-//     console.log(result[0]);
-//   }, []);
-//   return (
-//     <>
-//       <ItemDetail item={descripcion} />
-//     </>
-//   );
-// };
-// export default ItemDetailContainer;
-
 export default function ItemDetailcontainer() {
   const { producto_id } = useParams();
   // const { listProductos, setListProductos } = useState([]);
